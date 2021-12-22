d801279dcdc2c499c195d4ca7e85862305cfa961
arminjoya <94761705+armin1442@users.noreply.github.com>
2021-12-22 14:49:04 +0330

    Add files via upload

---
+++ tast1.py
@@ -0,0 +1,10 @@
+for i in range(4):
+    name = input()
+    score1 = float(input())
+    score2 = float(input())
+    score3 = float(input())
+    average = (score1 + score2 + score3)
+    print(average)
+
+       
+    
---
+++ tast9.py
@@ -0,0 +1,10 @@
+
+for i in range(44):
+    username = input()
+    password = input()
+
+    if username== "admin" and password== "asghersagsibil@2020":
+        print("welcome")
+
+    else:
+        print("Error  try again")    
