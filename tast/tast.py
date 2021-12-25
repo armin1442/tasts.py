c6fbc531162bbba8e270cd01afcbd8ac36c4a4e8
arminjoya <94761705+armin1442@users.noreply.github.com>
2021-12-25 19:34:53 +0330

    Add files via upload

---
+++ tast/tast.py
@@ -0,0 +1,13 @@
+a = input('how many people are you?')
+m = input('enter your grades?')
+
+k= a + m
+
+if a + m:
+    print(m)
+if m < a:
+    print(" number shom khob nist.")
+
+if m > a:
+    print('number shom khob ast.')
+    
