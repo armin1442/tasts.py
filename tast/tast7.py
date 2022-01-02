3f7184db5859c2628d975aceb24fe922e7502034
arminjoya <94761705+armin1442@users.noreply.github.com>
2022-01-02 13:17:54 +0330

    Add files via upload

---
+++ tast/tast7.py
@@ -0,0 +1,8 @@
+import random
+b=5
+while b == 5:
+    b=random.randint(1 , 7)
+    if b == 5:
+        print('*8*') 
+    else:
+        print("oh",b)    
---
+++ tast/tast8.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+b = int(input)
+
+while b :
+    b= int(input)
+    list1.apppend(b)
+
+print(List)    
